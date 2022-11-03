9a246bdec63c9054794a7946884a4e5f886f38eb
Christopher <cyu72@binghamton.edu>
2022-11-02 20:33:49 -0400

    middle commit

--- BST.cpp
+++ BST.cpp
@@ -1,2 +1,24 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include "BST.h"
 
+using namespace std;
+
+BST::BST(){
+    this->root = nullptr;
+}
+
+BST::BST(int value){
+    Node* newNode = new Node(value);
+    root = newNode;
+    currentNode = root;
+}
+
+// BST* BST::insert(int value){
+//     Node* newNode = new Node(value);
+//     if ((root == nullptr) || (root->getData() == -999999)) root = newNode;
+
+
+//     return this;
+    
+// }
--- BST.h
+++ BST.h
@@ -1,6 +1,5 @@
 #include <stdio.h>
 #include <stdlib.h>
-
 #pragma once
 
 using namespace std;
@@ -12,14 +11,21 @@ class BST{
                 int data;
                 Node* leftChild;
                 Node* rightChild;
-            Node(){int data = -999999; this->leftChild = nullptr; this->rightChild = nullptr;};
-            Node(int data){this->data = data;};
-            Node(const Node &otherNode){this->data = otherNode.data; this->leftChild = otherNode.leftChild; this->rightChild = otherNode.rightChild;};
+                Node(){int data = -999999; this->leftChild = nullptr; this->rightChild = nullptr;};
+                Node(int data){this->data = data; this->leftChild = nullptr; this->rightChild = nullptr;};
+                Node(const Node &otherNode){this->data = otherNode.data; this->leftChild = otherNode.leftChild; this->rightChild = otherNode.rightChild;};
+                int getData(){return this->data;};
         };
+        Node* root, *currentNode;
 
     public:
         BST();
+        BST(int value);
+        // BST* insert(int value); Incorrect
         ~BST();
+        void preOrderWalk();
+        void postOrderWalk();
+        void inOrderWalk();
     
 
 };
